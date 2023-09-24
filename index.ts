b5462f0875a6d4bd0f09bca08c930d6affe1c5e4
Lucas De Curtis <lucas.de-curtis@epitech.eu>
2023-09-24 15:04:05 +0900

    updated packages
    Signed-off-by: Lucas De Curtis <lucas.de-curtis@epitech.eu>

--- index.ts
+++ index.ts
@@ -4,28 +4,29 @@ export { zodResolver } from "@hookform/resolvers/zod";
 export {
   Controller,
   FormProvider,
+  UseControllerProps,
+  UseFormRegister,
   useController,
   useFieldArray,
   useForm,
   useFormContext,
   useFormState,
   useWatch,
-  UseControllerProps,
-  UseFormRegister,
 } from "react-hook-form";
 export type {
   Control,
   ControllerProps,
   ControllerRenderProps,
   DeepPartial,
+  DefaultValues,
   FieldError,
+  FieldErrors,
   FieldPath,
   FieldPathByValue,
   FieldPathValue,
   FieldValues,
   Path,
   UseFormReturn,
-  FieldErrors,
 } from "react-hook-form";
 
 export type TFieldName<TFieldValues extends FieldValues> = Path<TFieldValues>;
